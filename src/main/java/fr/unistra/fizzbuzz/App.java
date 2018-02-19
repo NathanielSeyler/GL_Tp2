4b6a489870ae0c952c68bb6fc330487ff7e63361
Nathaniel SEYLER <nathaniel.seyler@etu.unistra.fr>
2018-02-19 13:37:16 +0100

    implementation de FizzBuzz.java et de App.java

--- src/main/java/fr/unistra/fizzbuzz/App.java
+++ src/main/java/fr/unistra/fizzbuzz/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	int i;
+	FizzBuzz f = new FizzBuzz();
+	for(i=1;i<=100;i++)
+		System.out.println(f.convert(i));
     }
 }
--- src/main/java/fr/unistra/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/unistra/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,15 @@ package fr.unistra.fizzbuzz;
 
 public class FizzBuzz {
 
-	public String convert(int number) {
-		throw new RuntimeException("Not implemented");
+
+	public String convert(int number)
+	{
+		if(number % 15 == 0)
+			return "FizzBuzz";
+		if(number % 5 == 0)
+			return "Buzz";
+		if(number % 3 ==0)
+			return "Fizz";
+		return Integer.toString(number);
 	}
 }
