7d93d1d5de03c1eeb97dcbeef9c5e5801bceb7e6
Nathaniel SEYLER <nathaniel.seyler@etu.unistra.fr>
2018-02-23 21:53:01 +0100

    ajout de App.java (le main)

---
+++ src/main/java/fr/unistra/fizzbuzz/App.java
@@ -0,0 +1,17 @@
+package fr.unistra.fizzbuzz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		/*Displayer printer;
+		Int2String fizzbuzz = new FizzBuzz();
+		ProblemSolver solver = new ProblemSolver(printer,fizzbuzz);
+		solver.solve(100);*/
+        System.out.println( "Hello World!" );
+    }
+}
