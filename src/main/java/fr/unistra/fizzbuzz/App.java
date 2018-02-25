bf59ef40a38c185ba0486685b3bc3daf995d977e
Nathaniel SEYLER <nathaniel.seyler@etu.unistra.fr>
2018-02-25 13:10:32 +0100

    modif de App.java pour qu'il fasse l'affichage de fizzbuzz

--- src/main/java/fr/unistra/fizzbuzz/App.java
+++ src/main/java/fr/unistra/fizzbuzz/App.java
@@ -4,14 +4,18 @@ package fr.unistra.fizzbuzz;
  * Hello world!
  *
  */
-public class App 
+public class App implements Displayer
 {
+	public void display(String str)
+	{
+		System.out.println(str);
+	}
     public static void main( String[] args )
     {
-		/*Displayer printer;
+		Displayer printer = new App();
 		Int2String fizzbuzz = new FizzBuzz();
 		ProblemSolver solver = new ProblemSolver(printer,fizzbuzz);
-		solver.solve(100);*/
+		solver.solve(100);
         System.out.println( "Hello World!" );
     }
 }
